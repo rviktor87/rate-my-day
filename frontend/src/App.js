98c62d6b6b3e67bae9132227699f8a4cabf6a473
rviktor87 <rviktor87@gmail.com>
2022-06-17 19:44:20 +0200

    wrapping content to container

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,19 @@ import './App.scss';
 import Menu from "./components/Menu";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import { Routes, Route } from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
+import {Container} from "react-bootstrap";
 
 function App() {
   return (
     <>
-      <Menu />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-      </Routes>
+      <Menu/>
+      <Container>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="about" element={<About/>}/>
+        </Routes>
+      </Container>
     </>
 
   );
