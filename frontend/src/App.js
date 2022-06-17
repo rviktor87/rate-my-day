0869ba2d6c7cfeb62738fc2a9ac23f7d8b90dcbe
rviktor87 <rviktor87@gmail.com>
2022-06-17 19:24:50 +0200

    creating menu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
+import Menu from "./components/Menu";
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <Menu />
+    </>
 
-    </div>
   );
 }
 
---
+++ frontend/src/components/Menu.js
@@ -0,0 +1,20 @@
+import {Container, Nav, Navbar} from "react-bootstrap";
+
+function Menu() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">RateMyDay</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default Menu;
