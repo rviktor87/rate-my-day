95be4e936d57dddc9a7ff4309137e209c84efe4f
rviktor87 <rviktor87@gmail.com>
2022-06-17 19:39:27 +0200

    adding react router dom and navigating by it

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import './App.scss';
 import Menu from "./components/Menu";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Menu />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </>
 
   );
--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -1,15 +1,16 @@
 import {Container, Nav, Navbar} from "react-bootstrap";
+import {NavLink} from "react-router-dom";
 
 function Menu() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">RateMyDay</Navbar.Brand>
+        <Navbar.Brand as={NavLink} to="/">RateMyDay</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+            <Nav.Link as={NavLink} to="/about" >About</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
